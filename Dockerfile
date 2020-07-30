d8833e420bca8bd37faf0a3416ca8f60348dbfe8
Trevor Sullivan <trevortds3@gmail.com>
2020-07-30 13:45:30 -0600

    Switching to postgres

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ RUN apt-get update && apt-get install nginx vim -y --no-install-recommends
 COPY nginx.default /etc/nginx/sites-available/default
 RUN ln -sf /dev/stdout /var/log/nginx/access.log \
     && ln -sf /dev/stderr /var/log/nginx/error.log
+#RUN ln -sf /var/log/stdout /var/log/nginx/access.log \
+#    && ln -sf /var/log/stdout /var/log/nginx/error.log
 
 # Install necessary dependencies for spatialite (don't need this when switched to postgres)
 RUN apt-cache search mod_spatialite
@@ -24,6 +26,9 @@ WORKDIR /opt/app
 RUN pip install -r requirements.txt --cache-dir /opt/app/pip_cache
 RUN chown -R www-data:www-data /opt/app
 RUN chmod +x /opt/app/start-server.sh
+
+ARG DJANGO_SETTINGS_MODULE=geo.settings_deployment
+
 
 # start server
 EXPOSE 80
--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 # start-server.sh
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
-    (cd geo; python manage.py createsuperuser --no-input)
+
+echo "waiting for server to start"
+while !</dev/tcp/db/5432; do sleep 1; done;
+
+echo "Apply database migrations"
+(cd geo; python manage.py migrate auth)
+(cd geo; python manage.py migrate)
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] ; then
+#    (cd geo; python manage.py createsuperuser --no-input --email $DJANGO_SURERUSER_EMAIL \
+#            --username $DJANGO_SUPERUSER_USERNAME)
+    echo "Creating superuser"
+    (cd geo; python ./manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SURERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')")
 fi
-(cd geo; gunicorn geo.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+
+echo "collect static files"
+(cd geo; python manage.py collectstatic --noinput)
+
+
+echo "Start Server"
+(cd geo; gunicorn geo.wsgi  --user www-data --bind 0.0.0.0:8010 --workers 3 ) &
+#(cd geo; gunicorn geo.wsgi --log-level debug --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
+#  --user www-data --bind 0.0.0.0:8010 --workers 3 2>/var/log/stderr 1>/var/log/stdout) &
 nginx -g "daemon off;"
+
+#tail -qf --follow=name --retry /var/log/stdout /var/log/stderr
