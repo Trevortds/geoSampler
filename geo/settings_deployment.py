e1df98723eb1ca5b7f31ecd66ac28572a4936f19
Trevor Sullivan <trevortds3@gmail.com>
2020-08-04 13:21:56 -0600

    repairing docker build for automation

--- geo/settings_deployment.py
+++ geo/settings_deployment.py
@@ -56,3 +56,30 @@ DATABASES = {
     }
 }
 
+
+# TODO set up email logs, then delete this whole block
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'gunicorn': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
+            'maxBytes': 1024 * 1024 * 100,  # 100 mb
+        }
+    },
+    'loggers': {
+        'gunicorn.errors': {
+            'level': 'DEBUG',
+            'handlers': ['gunicorn'],
+            'propagate': True,
+        },
+    }
+}
